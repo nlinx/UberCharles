76c10d11d30046f48d068d8642cedbd18f1ae477
Richard Artoul <richardartoul@gmail.com>
2015-07-25 15:38:07 -0700

    [Feature] Login route will redirect to uber authorization

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ var errorHandler = require('./errorHandler');
 // var db = require('./db');
 var parser = require('body-parser');
 var redis = require('./redis');
+var uberApi = require('./uberApi');
+var OAuth = require('node-oauth');
 
 var express = require('express');
 var port = process.env.PORT || 3000;
@@ -14,6 +16,11 @@ app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/client'));
+
+//ROUTES
+app.get('/login', function(req, res) {
+  res.redirect('https://login.uber.com/oauth/authorize?client_id=' + uberApi.clientId + '&response_type=code');
+});
 
 //error handling middleware applied last
 app.use(errorHandler);
