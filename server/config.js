ad94c8abb34688797008a416bf5ca90baa2d6bd7
Richard Artoul <richardartoul@gmail.com>
2015-07-26 03:03:24 -0700

    [Refactor] Fixed a small typo

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  UBER_CLIENT_ID: proccess.env.HEROKU_UBER_CLIENT_ID,
-  UBER_SERVER_TOKEN: proccess.env.HEROKU_SERVER_TOKEN,
-  UBER_SECRET: proccess.env.HEROKU_UBER_SECRET,
-  WIT_KEY: proccess.env.HEROKU_WIT_KEY
+  UBER_CLIENT_ID: process.env.HEROKU_UBER_CLIENT_ID,
+  UBER_SERVER_TOKEN: process.env.HEROKU_SERVER_TOKEN,
+  UBER_SECRET: process.env.HEROKU_UBER_SECRET,
+  WIT_KEY: process.env.HEROKU_WIT_KEY
 };
